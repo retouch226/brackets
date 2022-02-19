91419af0deb25daa2f89f21588537ee0fde99456
Ivan <retouch226@gmail.com>
2022-02-19 19:07:57 +0200

    feat: 12/20

--- src/index.js
+++ src/index.js
@@ -1,26 +1,90 @@
 //module.exports =
-module.exports = function check(str, bracketsConfig) {
-  let stack = [];
-  str = str.split("");
-  for (let index = 0; index < str.length; index++) {
-    if (stack.length !== 0 && str[index] !== stack[stack.length - 1]) {
-      str.splice(str[index], 1);
-      stack.pop();
-      index--;
-    } else if (str[index] === str[index + 1]) {
-      stack.push(str[index]);
-      str.splice(str[index], 1);
 
-      index--;
-    } else if (str[index] !== str[index + 1]) {
-      str.splice(index, 2);
-      index--;
+module.exports = function check(str, bracketsConfig) {
+  let stackScope = [];
+  let slashScope = [];
+  let figureScope = [];
+  str = str.split("");
+  const types = ["(", ")", "|", "{", "}", "[", "]"];
+  for (let index = 0; index < str.length; index++) {
+    if (index != str.length - 1) {
+      if (str[index] === "(" || str[index] === ")") {
+        if (
+          stackScope.length !== 0 &&
+          str[index] !== stackScope[stackScope.length - 1]
+        ) {
+          str.splice(str[index], 1);
+          stackScope.pop();
+          index--;
+          continue;
+        } else if (str[index] === str[index + 1]) {
+          stackScope.push(str[index]);
+          str.splice(str[index], 1);
+          index--;
+          continue;
+        } else if (str[index] === "(" && str[index] !== str[index + 1]) {
+          str.splice(index, 2);
+          index--;
+          continue;
+        }
+      }
+      if (str[index] === "|") {
+        if (
+          slashScope.length !== 0 &&
+          str[index] === slashScope[slashScope.length - 1]
+        ) {
+          str.splice(str[index], 1);
+          slashScope.pop();
+          index--;
+          continue;
+        }
+        if (str[index] === str[index + 1]) {
+          str.splice(index, 2);
+          index--;
+          continue;
+        } else if (str[index] !== str[index + 1]) {
+          slashScope.push(str[index]);
+          str.splice(str[index], 1);
+          index--;
+          continue;
+        }
+      }
+    } else {
+      break;
     }
   }
-  if (stack.length === 0) {
+  if (stackScope.length === 0 && str.length === 0 && slashScope.length === 0) {
     return true;
   }
   return false;
 };
 
-//console.log(check("((()))()", []));
+///////primitive
+// function check(str, bracketsConfig) {
+//   let stackScope = [];
+//   str = str.split("");
+//   for (let index = 0; index < str.length; index++) {
+//     if (index != str.length - 1) {
+//       if (stackScope.length !== 0 && str[index] !== stackScope[stackScope.length - 1]) {
+//         str.splice(str[index], 1);
+//         stackScope.pop();
+//         index--;
+//       } else if (str[index] === str[index + 1]) {
+//         stackScope.push(str[index]);
+//         str.splice(str[index], 1);
+//         index--;
+//       } else if (str[index] !== str[index + 1]) {
+//         str.splice(index, 2);
+//         index--;
+//       }
+//     } else {
+//       break;
+//     }
+//   }
+//   if (stackScope.length === 0 && str.length === 0) {
+//     return true;
+//   }
+//   return false;
+// }
+
+//console.log(check("|()|(||)||", []));
